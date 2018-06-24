cbaa4549ccc532197dd26f3ee973ce15b08d8a56
sarirahman <trikurniasari05@gmail.com>
2018-06-24 23:08:58 +0700

    model

--- application/models/mTandaTerimaTA.php
+++ application/models/mTandaTerimaTA.php
@@ -1,14 +1,21 @@
 <?php
 class MTandaTerimaTA extends CI_Model{
 
+	public function __construct()
+	{
+		parent::__construct();
+		$this->data['table_name'] 	= 'tanda_terima_ta';
+	}
+
 	function TandaTerimaTA_list(){
 		$hasil=$this->db->query("SELECT tanda_terima_ta.nim, tanda_terima_ta.nama, tanda_terima_ta.tanggal FROM tanda_terima_ta");
 		return $hasil->result();
 	}
 
-	function simpan_TandaTerimaTA($nimTTTA,$namaTTTA,$tglTTTA){
-		$hasil=$this->db->query("INSERT INTO tanda_terima_ta (nim,nipus,nama,tanggal)VALUES('$nimTTTA','09021181520025','$namaTTTA','$tglTTTA')");
-		return $hasil;
+	function simpan_TandaTerimaTA($data){
+		// $hasil=$this->db->query("INSERT INTO tanda_terima_ta (nim,nipus,nama,tanggal)VALUES('$nimTTTA','09021181520025','$namaTTTA','$tglTTTA')");
+		$query = $this->db->insert($this->data['table_name'],$data);
+		return $query;
 	}
 
 	function get_TandaTerimaTA_by_kode($nimTTTA){
@@ -25,8 +32,9 @@ class MTandaTerimaTA extends CI_Model{
 		return $hasil;
 	}
 
-	function update_TandaTerimaTA($nimTTTA,$namaTTTA,$tglTTTA){
-		$hasil=$this->db->query("UPDATE tanda_terima_ta SET nama='$namaTTTA',tanggal='$tglTTTA' WHERE nim='$nimTTTA'");
+	function update_TandaTerimaTA($nimTTTA,$data){
+		$this->db->WHERE('NIM', $nimTTA);
+		$hasil=$this->db->update($this->data['table_name'], $data);
 		return $hasil;
 	}
 
